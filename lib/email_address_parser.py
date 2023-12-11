cb6fbebceef335948049d73c218c8fb4033dcca5
Irene Annah <irene.gitau@strathmore.edu>
2023-12-10 23:44:23 -0800

    Update email_address_parser.py
    done

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,14 @@
 # your code goes here!
+# lib/email_address_parser.py
+
+class EmailAddressParser:
+    def __init__(self):
+        pass 
+
+    def parse_email_address(self, email):
+        username, domain = email.split('@')
+        return {'username': username, 'domain': domain}
+
+
+if __name__ == '__main__':
+    import ipdb; ipdb.set_trace()
